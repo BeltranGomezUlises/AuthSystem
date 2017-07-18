8d626cc2701e5cbbbda3de206b04ddc44d9cfc05
ulises <beltrangomezulises@gmail.com>
2017-07-17 22:21:40 -0600

    Documentacion Automatica

--- src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -31,6 +31,7 @@ public class ApplicationConfig extends Application {
         resources.add(com.machineAdmin.services.cg.administracion.Permisos.class);
         resources.add(com.machineAdmin.services.cg.administracion.Usuarios.class);
         resources.add(com.machineAdmin.services.cg.commons.ServiceCommons.class);
+        resources.add(com.machineAdmin.services.cg.commons.ServiceFacade.class);
     }
     
 }
